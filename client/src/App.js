cd4d78586977b572d6b73c1c638089f556a615d0
JoroRaynov <jornalo891@gmail.com>
2023-04-11 16:37:36 +0300

    Add css modules

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Header from './Components/Header/Header';
 import {Register} from './Components/Register/Register';
 import { Login } from './Components/Login/Login';
+import { Home } from './Components/Home/Home';
 
 
 function App() {
   return (
     <>
       <Header />
-
+      <Home />
       <main>
         <Routes>
             <Route path={"/auth/register"} element={<Register />} />
