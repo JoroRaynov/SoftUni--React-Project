cd70b008c91ef34e14f8612249905dd65a6c6f7e
JoroRaynov <jornalo891@gmail.com>
2023-04-11 14:46:22 +0300

    Create Login and Register Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css';
 import Header from './Components/Header/Header';
+import {Register} from './Components/Register/Register';
+import { Login } from './Components/Login/Login';
+
 
 function App() {
   return (
     <>
-        <Header />
+      <Header />
+
+      <main>
+        <Routes>
+            <Route path={"/auth/register"} element={<Register />} />
+            <Route path={"/auth/login"}  element = {<Login />} />
+           
+        </Routes>
+      </main>
     </>
   );
 }
--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,19 +1,38 @@
+import { Link } from "react-router-dom";
+
 import './Header.css'
 
-export default function Header (){
+export default function Header() {
 
- return (
-    <header className="header">
-        <nav className="nav">
-            <img className="logo" src={require ("./olx-logo.png")} alt="olx-logo" />
-            <ul className="listItems">
-                <li className="listItem">Съобщения</li>
-                <li className="listItem">3</li>
-                <li className="listItem">Твоят Профил</li>
-                <button className="primary addAd">Добави обява</button>
-            </ul>
-        </nav>
-    </header>
-);
+    return (
+        <header className="header">
+            <nav className="nav">
+
+                <Link to={"/"} className="message" >
+                    <img className="logo" src={require("./olx-logo.png")} alt="olx-logo" />
+                </Link >
+                <ul className="listItems">
+                    <li className="listItem">
+                        <i className="fa-regular fa-comment"></i>
+                        <Link to={"/"} className="message" >Съобщения</Link >
+                    </li>
+                    <li className="listItem">
+                        <Link to={"/"} className="message" ><i className="fa-regular fa-heart"></i>
+                        </Link >
+                    </li>
+                    <li className="listItem">
+                        <Link to={"/auth/login"} className="message" >Влез</Link >
+                    </li>
+                    <li className="listItem">
+                        <Link to={"/auth/register"} className="message" >Регистрирай се</Link >
+                    </li>
+                    <li className="listItem">
+                        <Link to={"/"} className="message" >Твоят Профил</Link >
+                    </li>
+                    <Link to={"/"} className="primary addAd">Добави обява</Link>
+                </ul>
+            </nav>
+        </header>
+    );
 
 }
